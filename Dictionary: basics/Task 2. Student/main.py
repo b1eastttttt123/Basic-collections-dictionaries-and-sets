eba2dac3e4b1ed2f3d54ce07bea57a969ae8bd86
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-04-18 00:45:16 +1000

    Update main.py

--- Dictionary: basics/Task 2. Student/main.py
+++ Dictionary: basics/Task 2. Student/main.py
@@ -1,1 +1,19 @@
+students_info_enter = input(
+    "Введите информацию о студенте через пробел\n"
+    "(имя, фамилия, город, место учёбы, оценки): "
+)
 
+students_info = students_info_enter.split()
+student = dict()
+
+student["Имя"] = students_info[0]
+student["Фамилия"] = students_info[1]
+student["Город"] = students_info[2]
+student["Место учёбы"] = students_info[3]
+student["Оценки"] = []
+
+for estimation in students_info[4:]:
+    student["Оценки"].append(int(estimation))
+
+for result in student:
+    print(result, "-", student[result])
