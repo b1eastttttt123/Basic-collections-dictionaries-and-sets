710f98e8bc1bdf69de9c171fcf60a6a433279c4e
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-04-18 01:11:25 +1000

    Update main.py

--- Dictionary: basics/Task 3. Contacts/main.py
+++ Dictionary: basics/Task 3. Contacts/main.py
@@ -1,1 +1,15 @@
+result = dict()
+print('Текущие контакты на телефоне: \n<Пусто>')
+
+while True:
+    name = input('Введите имя: ')
+    number_phone = int(input('Введите номер телефона: '))
+    if name not in result:
+        result[name] = number_phone
+        print('Текущие контакты на телефоне: \n')
+        for info in result:
+            print(info, result[info])
+    else:
+        print('Ошибка: такое имя уже существует.')
+        break
 
